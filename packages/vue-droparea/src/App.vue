574155f68115ab5210f90ef37b4ba8d8f31c76f2
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-11-22 20:03:19 +0900

    chore: initial commit

---
+++ packages/vue-droparea/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  import HelloWorld from '/@/components/HelloWorld.vue'
+
+  export default defineComponent({
+    name: 'App',
+    components: {
+      HelloWorld,
+    },
+  })
+</script>
