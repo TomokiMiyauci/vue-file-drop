d35180f46bf10c1927df328f2e176f70e06f6f5c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-11-25 23:16:14 +0900

    chore: change config

--- packages/vue-droparea/vite.config.ts
+++ packages/vue-droparea/vite.config.ts
@@ -5,6 +5,15 @@ const config: UserConfig = {
   alias: {
     '/@/': join(__dirname, 'src'),
   },
+
+  rollupInputOptions: {
+    input: 'src/wrapper.ts',
+  },
+
+  rollupOutputOptions: {
+    name: 'MyComponent',
+    exports: 'named',
+  },
 }
 
 export default config
