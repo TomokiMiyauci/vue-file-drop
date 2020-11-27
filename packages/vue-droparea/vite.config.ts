b5c5275e0dfb24b7f7e6014e70a335ed9ea96343
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-11-28 02:08:11 +0900

    chore(vite.config.ts): rollback settings

--- packages/vue-droparea/vite.config.ts
+++ packages/vue-droparea/vite.config.ts
@@ -5,16 +5,6 @@ const config: UserConfig = {
   alias: {
     '/@/': join(__dirname, 'src'),
   },
-
-  rollupInputOptions: {
-    input: 'src/wrapper.ts',
-  },
-
-  rollupOutputOptions: {
-    name: 'MyComponent',
-    exports: 'named',
-    entryFileNames: 'vue-droparea.js',
-  },
 }
 
 export default config
