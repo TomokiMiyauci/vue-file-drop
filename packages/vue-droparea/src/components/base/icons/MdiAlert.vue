9841b04892bc50e28c13e72051205e084bdacc48
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-11-29 00:15:39 +0900

    feat(icons/*): add icons

---
+++ packages/vue-droparea/src/components/base/icons/MdiAlert.vue
@@ -0,0 +1,18 @@
+<template>
+  <base-svg>
+    <path :d="mdiAlert" />
+  </base-svg>
+</template>
+
+<script lang="ts">
+  import { mdiAlert } from '@mdi/js'
+  import { defineComponent } from 'vue'
+  import BaseSvg from '/@/components/base/BaseSvg.vue'
+
+  export default defineComponent({
+    components: { BaseSvg },
+    setup() {
+      return { mdiAlert }
+    },
+  })
+</script>
---
+++ packages/vue-droparea/src/components/base/icons/MdiCheckCircle.vue
@@ -0,0 +1,18 @@
+<template>
+  <base-svg>
+    <path :d="mdiCheckCircle" />
+  </base-svg>
+</template>
+
+<script lang="ts">
+  import { mdiCheckCircle } from '@mdi/js'
+  import { defineComponent } from 'vue'
+  import BaseSvg from '/@/components/base/BaseSvg.vue'
+
+  export default defineComponent({
+    components: { BaseSvg },
+    setup() {
+      return { mdiCheckCircle }
+    },
+  })
+</script>
